4470e3a9e3a65c0c4de851de296a61049e800ccf
aravindkarri <aravindkarri98495@gmail.com>
2021-06-26 14:37:36 +0530

    ADDED: Perfect number logic developed

---
+++ src/com/bridgelabz/logical_programming_problems/perfectnumber/PerfectNumber.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.logical_programming_problems.perfectnumber;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	public static void main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number to check pefect number on not");
+		int number = scanner.nextInt();
+		scanner.close();
+		int factorsSum=0;
+		for(int i=1;i<=number/2;i++)
+		{
+			if (number%i == 0)
+			{
+				factorsSum += i;
+			}
+		}
+		if (factorsSum == number)
+		{
+			System.out.println("It is a perfect number");
+		
+		}
+		else {
+			System.out.println("it is not a perfect number");
+		}
+	}
+
+}
