49f87523a964683c4bcd1cfca83ba6b665d9abd6
aravindkarri <aravindkarri98495@gmail.com>
2021-06-27 20:20:15 +0530

    ADDED: find the minimum number of notes required for given amount

---
+++ src/com/bridgelabz/logical_programming_problems/vendingmachine/VendingMachine.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.logical_programming_problems.vendingmachine;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+
+	public static void main(String[] args) {
+		
+		int notes[] = {1,2,5,10,50,100,500,1000};
+		int noteCounter[] = new int[notes.length];
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter amount to calculte minimum number of notes to get return :");
+		int amount = scanner.nextInt();
+		scanner.close();
+		int minNumOfNotes=0;
+		for (int index=notes.length-1;index >= 0;index--)
+		{
+			if (amount >= notes[index])
+			{
+				noteCounter[index] = amount/notes[index];
+				amount = amount - noteCounter[index]*notes[index];
+			}
+		}
+		for (int index=notes.length-1;index>=0;index--)
+		{
+			if (noteCounter[index] != 0)
+			{
+				minNumOfNotes++;
+				System.out.println(notes[index] + " : "+noteCounter[index]);
+			}
+		}
+		System.out.println("minimum number of notes required are : "+minNumOfNotes);
+	}
+
+}
